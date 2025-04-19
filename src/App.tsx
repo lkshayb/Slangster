1252e03781aac3c36f838202f6d7a009330dab2d
Lakshay Bhatia <bhatia00lakshay@gmail.com>
2025-04-19 18:57:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,6 @@ function App() {
         </div>
         
 
-        {/* Input Section - keeping the existing input code */}
         <div className='fixed bottom-0 w-full bg-white border-t border-gray-200 p-4 shadow-lg'>
           <div className='max-w-4xl mx-auto flex gap-3'>
             <input 
