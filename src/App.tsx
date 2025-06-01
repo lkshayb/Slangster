fb5f497f7710399d54b9b23d51240ed05a16d522
lakshay bhatia <bhatia00lakshay@gmail.com>
2025-06-01 13:15:42 +0530

    Fixed issue #3

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,7 @@ function App() {
       return <div className='fixed bottom-0 w-full bg-white border-t border-gray-200 p-4 shadow-lg'>
       <div className='max-w-4xl mx-auto flex gap-3 '>
         <input ref={chatref} type="text" placeholder='Type your message [Shift + Enter]' onKeyDown={SendOnEnter} className='flex-1 px-4 py-2 rounded-full border border-gray-400 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 transition-all shadow-xl'/>
-        <button onClick={handleSubmit} className='px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition-colors duration-200 flex items-center gap-2 shadow-lg shadow-blue-500/50 '>
+        <button onClick={handleSubmit} disabled={isload} className={`${isload ? "cursor-not-allowed" : "cursor-pointer"} px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition-colors duration-200 flex items-center gap-2 shadow-lg shadow-blue-500/50 `}>
           Send
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
