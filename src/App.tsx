fde84dcaf695d435d9d6265db134c5dd2ef38ad9
Lakshay Bhatia <b.lakshay.aspect@gmail.com>
2025-10-03 22:19:03 +0530

    minor ui fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState ,useEffect} from 'react'
 import Markdown from 'react-markdown';
+import {Send} from 'lucide-react';
 import './App.css'
 import { GoogleGenAI } from "@google/genai";
 import { SyncLoader } from 'react-spinners';
@@ -227,18 +228,17 @@ function App() {
       return <div className='fixed bottom-0 w-full bg-white border-t border-gray-200 p-4 shadow-lg'>
         
       <div className='max-w-4xl mx-auto flex gap-3  items-center'>
+        
+        <input ref={chatref} type="text" placeholder='Type your message' onKeyDown={SendOnEnter} className='flex-1 px-4 py-2 rounded-xl outline-none   transition-all bg-black/10 placeholder-gray-700 caret-white   text-xl'/>
+        <button onClick={handleSubmit} disabled={isload} className={` group ${isload ? "cursor-not-allowed" : "cursor-pointer"} px-6 py-2 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition-colors duration-200 flex items-center gap-2 shadow-lg shadow-blue-500/50 `}>
+          Send
+          <Send className='h-5 w-5 group-hover:rotate-[20deg] duration-500 '/>
+        </button> 
         {selectedImage && (<img src={URL.createObjectURL(selectedImage)} className='w-[50px] h-[50px] rounded-2xl' />)}
         <label  htmlFor="file-upload"  className="cursor-pointer h-7 mr-5 w-7 text-gray-500 hover:text-blue-600 items-center">
           <img src="./image.png" alt="" />
         </label>
         <input accept="image/*" id="file-upload" onChange={handleImageChange} type="file" className="hidden" />
-        <input ref={chatref} type="text" placeholder='Type your message [Shift + Enter]' onKeyDown={SendOnEnter} className='flex-1 px-4 py-2 rounded-full border border-gray-400 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 transition-all shadow-xl'/>
-        <button onClick={handleSubmit} disabled={isload} className={`${isload ? "cursor-not-allowed" : "cursor-pointer"} px-6 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition-colors duration-200 flex items-center gap-2 shadow-lg shadow-blue-500/50 `}>
-          Send
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-          </svg>
-        </button>
       </div>
     </div>
     }
